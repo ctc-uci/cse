18042e57a0f50eae228ecbc29081fb40a78fd1cc
Angelina Wang <wangangeliina@gmail.com>
2025-02-05 20:33:24 -0800

    finished resource ui

--- client/src/components/forms/createArticle.tsx
+++ client/src/components/forms/createArticle.tsx
@@ -1,20 +1,24 @@
 import {
     Box,
     Button,
+    Checkbox,
     FormControl,
     FormLabel,
     FormErrorMessage,
     Input,
+    Stack,
+    VStack,
   } from '@chakra-ui/react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import { useState } from "react";
 import { z } from "zod";
 
 const createArticleSchema = z.object({
     photo: z.string(),
     link: z.string().url("Please enter a valid url."),
-    tag: z.string().min(1, "You must include at least one tag."),
+    tag: z.array(z.string()).min(1, "You must include at least one tag."),
     textInput: z.string().min(1, "Please write a description of the article."),
     title: z.string().min(1, "Your article must include a title.")
 });
@@ -25,6 +29,7 @@ const CreateArticle = () => {
     const {
         register,
         handleSubmit,
+        setValue,
         formState: { errors },
     } = useForm<ArticleFormValues>({
         resolver: zodResolver(createArticleSchema),
@@ -47,14 +52,44 @@ const CreateArticle = () => {
         }
     };
 
+    const [selectTags, setSelectTags] = useState<string[]>([]);
+
+    const tags = ["Ballet", "Classical", "Custom"];
+
+    const handleTags = (tag: string) => {
+        let newTags;
+        if (selectTags.includes(tag)) {
+            newTags = selectTags.filter((t) => t !== tag);
+        } else {
+            newTags = [...selectTags, tag]
+        }
+
+        setSelectTags(newTags);
+        setValue("tag", newTags, {shouldValidate: true});
+    }
+
     return (
+    <VStack
+      spacing={8}
+      sx={{ width: 300, marginX: "auto" }}
+    >
         <Box display="flex" justifyContent="center" alignItems="center">
             <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl>
+            <FormControl isInvalid={!!errors.photo}>
                 <FormLabel>Select media to upload.</FormLabel>
                 <Input
                     placeholder="Photo"
+                    size={"lg"}
+                    {...register("photo")}
+                    name="photo"
+                    isRequired
+                    autoComplete="photo"
                     ></Input>
+                <FormErrorMessage>
+                    {errors.photo?.message?.toString()}
+                </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={!!errors.link}>
                 <Input
                     placeholder="Link"
                     type="link"
@@ -64,16 +99,23 @@ const CreateArticle = () => {
                     isRequired
                     autoComplete="link"
                 ></Input>
+                <FormErrorMessage>{errors.link?.message?.toString()}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={!!errors.tag}>
                 <FormLabel>Select tags for media.</FormLabel>
-                <div>
-                    <Button>Ballet</Button>
-                    <Button>Classical</Button>
-                    <Button>Custom</Button>
-                </div>
+                <Stack direction="row">
+                    {tags.map((tag) => (
+                         <Button
+                         key={tag}
+                        onClick={() => handleTags(tag)}
+                     >
+                         {tag}
+                     </Button>
+                    ))}
+                </Stack>
+                <FormErrorMessage>{errors.tag?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={!!errors.title}>
                 <FormLabel>Add a title.</FormLabel>
                 <Input
                     placeholder="Add title here."
@@ -84,16 +126,18 @@ const CreateArticle = () => {
                     isRequired
                     autoComplete="title"
                     ></Input>
-                <FormErrorMessage>Title is required.</FormErrorMessage>
+                <FormErrorMessage>{errors.title?.message?.toString()}</FormErrorMessage>
             </FormControl>
                 <Button
                     type="submit"
                     size={"lg"}
                     sx={{width: "100% "}}
+                    isDisabled={Object.keys(errors).length > 0}
                 >Next</Button>
                 {/* insert review section */}
             </form>
         </Box>
+    </VStack>
   )
 }
 
